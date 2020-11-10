1b027999209a03ac7b12fd9ca31c72abfd80ba19
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-11-10 16:33:53 +0100

    Defining the alien position

--- js/main.js
+++ js/main.js
@@ -11,4 +11,39 @@ document.addEventListener("DOMContentLoaded", () => {
   let result = 0;
   let direction = 1; //the direction we want to go
   let invaderId = null;
+
+  //Defining the alien invaders
+  //Indexes we want our invaders to be in
+  const alienInvaders = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    30,
+    31,
+    32,
+    33,
+    34,
+    35,
+    36,
+    37,
+    38,
+    39,
+  ];
 });
