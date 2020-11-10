3b4b1032e7b655c5be942f973d62c3480aa45431
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-11-10 16:31:47 +0100

    Pick up classes and ids with querySelectors

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 "use strict";
+/*El evento DOMContentLoaded es disparado cuando el documento HTML ha sido completamente cargado*/
+document.addEventListener("DOMContentLoaded", () => {
+  //Pick up classes and ids with querySelectors
+  const squares = document.querySelectorAll(".grid div");
+  const resultDisplay = document.querySelector("#result");
+  let width = 15; //we want the width of the grid to be 15
+  let currentShooterIndex = 202; //we want the shooter to start at index 202
+  let currentInvaderIndex = 0; //we want our invader to start at index 0
+  let alienInvadersTakenDown = []; //invaders we have shot down
+  let result = 0;
+  let direction = 1; //the direction we want to go
+  let invaderId = null;
+});
